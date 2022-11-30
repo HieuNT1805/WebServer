cb542c8a7aa50b6bce1f207eff7565d9c60ae0fd
ThaiNguyen54 <91075598+kimchi9199@users.noreply.github.com>
2022-12-01 00:47:00 +0700

    update code

--- manager/customer_manager.js
+++
@@ -1,9 +0,0 @@
-// const BCrypt = require('bcryptjs');
-// // encrypt password
-// const Validator = require('validator');
-// const JsonWebToken = require('jsonwebtoken');
-// // our components
-// const Customer = require('../models/customer.js');
-// const MongoDBConfig = require('../config/MongoDBConfig.js');
-//
-
